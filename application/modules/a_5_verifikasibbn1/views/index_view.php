224dfe2b014a2b7333438d8a28a9916909f7f025
edot92 <edot92@gmail.com>
2016-09-21 11:23:42 +0700

    add firstview verifikasi bbn1

---
+++ application/modules/a_5_verifikasibbn1/views/index_view.php
@@ -0,0 +1,351 @@
+<!-- registrasi container-fluid-->
+<div class="container-fluid" style="background-color: rgb(255, 255, 255);">
+
+  <div class="row">
+    <br>
+    <form class="form-horizontal">
+      <div style="margin-bottom: 5px" class="form-group col-sm-8">
+        <label for="inputEmail3" class=" control-label col-sm-3">No. BPKB</label>
+        <div class="col-sm-9">
+          <input type="email" class="form-control" id="inputEmail3" placeholder="No. BPKB">
+        </div>
+      </div>
+      <div style="margin-bottom: 5px" class="form-group col-sm-8">
+        <label for="inputPassword3" class=" control-label col-sm-3">No. Rangka</label>
+        <div class="col-sm-9">
+          <input type="password" class="form-control " id="inputPassword3" placeholder="No. Rangka">
+        </div>
+      </div>
+      <div style="margin-bottom: 5px" class="form-group col-sm-8">
+        <label for="inputPassword3" class=" control-label col-sm-3">Barcode Berkas</label>
+        <div class="col-sm-9">
+          <input type="password" class="form-control" id="inputPassword3" placeholder="Barcode Berkas">
+        </div>
+      </div>
+    </form>
+    <a href="#" class="btn btn-primary btn-lg" role="button"><span class="glyphicon glyphicon-search"></span> <br />Cari Data</a>
+    <a href="#" class="btn btn-primary btn-lg" role="button"><span class="glyphicon glyphicon-repeat"></span> <br />Baru</a>
+  </div>
+  <hr style="margin:10px">
+  <div class="row">
+    <form class="form-horizontal">
+      <div style="margin-bottom: 5px" class="form-group col-sm-8">
+        <label for="inputPassword3" class=" control-label col-sm-3">Tanggal Verifikasi</label>
+        <div class="col-sm-9">
+          <input type="password" class="form-control" id="inputPassword3" placeholder="Tanggal Verifikasi">
+        </div>
+      </div>
+    </form>
+    <a href="#" class="btn btn-primary btn-lg" role="button"><span class="glyphicon glyphicon-ok"></span> <br />Verifikasi</a>
+  </div>
+  <hr style="margin:10px">
+  <div class="row">
+    <form class="form-horizontal">
+      <div class="form-group">
+        <label for="inputPassword3" class=" control-label col-sm-2">No. BPKB</label>
+        <div class="col-sm-2">
+          <input type="password" class="form-control" id="inputPassword3" placeholder="No. BPKB">
+        </div>
+        <label for="inputPassword3" class=" control-label col-sm-1">Tempat</label>
+        <div class="col-sm-3">
+          <input type="password" class="form-control" id="inputPassword3" placeholder="Tempat">
+        </div>
+      </div>
+    </form>
+  </div>
+
+
+
+
+
+
+
+
+  <!-- start tabs -->
+  <!-- <div class="row">
+  <div  id='idTabsVeriBBN1' style="position: relative; width:100%;">
+  <ul style='margin-left: 20px;'>
+  <li>I. Identitas Pemiliki</li>
+  <li>II. Identitas Kendaraan</li>
+  <li>III. Dok. Reg Pertama</li>
+  <li>IIV. Dikeluarkan Oleh / No. Reg</li>
+</ul> -->
+
+<!-- start tabs I -->
+<!-- <div style="overflow: auto; border: none;"  style="width: 100%; height: 100%">
+
+
+<!-- </div> -->
+<!-- end tabs 1 -->
+
+<!-- start tabs II -->
+<!-- <div style="overflow: auto; border: none;"  style="width: 100%; height: 100%">  -->
+<!--    Content 2-->
+
+<!-- </div>  -->
+<!-- end tabs II -->
+
+<!-- </div>
+</div> -->
+<!-- end tabs -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div id='idTabsVeriBBN1'  style="position: relative; width:100%;">
+  <ul style='margin-left: 20px;'>
+    <li>I. Identitas Pemiliki</li>
+    <li>II. Identitas Kendaraan</li>
+    <li>III. Dok. Reg Pertama</li>
+    <li>IIV. Dikeluarkan Oleh / No. Reg</li>
+
+  </ul>
+  <div style="overflow: auto; border: none;"  style="width: 100%; height: 100%">
+    <div class="col-md-12">
+      <br>
+      <form class="form-horizontal" >
+        <div style="margin-bottom: 5px" class="form-group">
+          <label for="inputEmail3" class=" control-label col-sm-2">Nama Pemilik</label>
+          <div class="col-sm-8">
+            <input type="email" class="form-control" id="inputEmail3" placeholder="Nama Pemilik">
+          </div>
+        </div>
+        <div style="margin-bottom: 5px" class="form-group">
+          <label for="inputPassword3" class=" control-label col-sm-2">Pekerjaan</label>
+          <div class="col-sm-8">
+            <input type="password" class="form-control" id="inputPassword3" placeholder="Pekerjaan">
+          </div>
+        </div>
+        <div style="margin-bottom: 5px" class="form-group">
+          <label for="inputPassword3" class=" control-label col-sm-2">Alamat</label>
+          <div class="col-sm-8">
+            <textarea type="password" class="form-control input-style" id="inputPassword3" placeholder="Alamat"></textarea>
+          </div>
+        </div>
+        <div style="margin-bottom: 5px" class="form-group">
+          <label for="inputPassword3" class=" control-label col-sm-2">No. KTP</label>
+          <div class="col-sm-8">
+            <input type="password" class="form-control" id="inputPassword3" placeholder="No. KTP">
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+
+  <div style="overflow: auto; border: none;"  style="width: 100%; height: 100%">    <!-- Content 2 -->
+    <br>
+    <div class="col-md-6">
+      <form class="form-horizontal" col-sm-2>
+        <div style="margin-bottom: 5px" class="form-group">
+          <label for="inputEmail3" class=" control-label col-sm-4">Nomor Registrasi</label>
+          <div class="col-sm-6">
+            <input type="email" class="form-control" id="inputEmail3" placeholder="Nomor Registrasi">
+          </div>
+        </div>
+        <div style="margin-bottom: 5px" class="form-group">
+          <label for="inputPassword3" class=" control-label col-sm-4">Merk</label>
+          <div class="col-sm-6">
+            <input type="password" class="form-control" id="inputPassword3" placeholder="Merk">
+          </div>
+        </div>
+        <div style="margin-bottom: 5px" class="form-group">
+          <label for="inputPassword3" class=" control-label col-sm-4">Type</label>
+          <div class="col-sm-6">
+            <input type="password" class="form-control" id="inputPassword3" placeholder="Type">
+          </div>
+        </div>
+        <div style="margin-bottom: 5px" class="form-group">
+          <label for="inputPassword3" class=" control-label col-sm-4">Jenis</label>
+          <div class="col-sm-6">
+            <input type="password" class="form-control" id="inputPassword3" placeholder="Jenis">
+          </div>
+        </div>
+        <div style="margin-bottom: 5px" class="form-group">
+          <label for="inputPassword3" class=" control-label col-sm-4">Model</label>
+          <div class="col-sm-6">
+            <input type="password" class="form-control" id="inputPassword3" placeholder="Model">
+          </div>
+        </div>
+        <div style="margin-bottom: 5px" class="form-group">
+          <label for="inputPassword3" class=" control-label col-sm-4">Tahun Pembuatan</label>
+          <div class="col-sm-6">
+            <input type="password" class="form-control" id="inputPassword3" placeholder="Tahun Pembuatan">
+          </div>
+        </div>
+        <div style="margin-bottom: 5px" class="form-group">
+          <label for="inputPassword3" class=" control-label col-sm-4">Isi Silinder</label>
+          <div class="col-sm-6">
+            <input type="password" class="form-control" id="inputPassword3" placeholder="Isi Silinder">
+          </div>
+        </div>
+      </form>
+    </div>
+    <div class="col-md-6">
+      <form class="form-horizontal" col-sm-2>
+        <div style="margin-bottom: 5px" class="form-group">
+          <label for="inputEmail3" class=" control-label col-sm-4">Nomor Rangka</label>
+          <div class="col-sm-6">
+            <input type="email" class="form-control" id="inputEmail3" placeholder="Nomor Rangka">
+          </div>
+        </div>
+        <div style="margin-bottom: 5px" class="form-group">
+          <label for="inputEmail3" class=" control-label col-sm-4">Nomor Mesin</label>
+          <div class="col-sm-6">
+            <input type="email" class="form-control" id="inputEmail3" placeholder="Nomor Mesin">
+          </div>
+        </div>
+        <div style="margin-bottom: 5px" class="form-group">
+          <label for="inputEmail3" class=" control-label col-sm-4">Bahan Bakar</label>
+          <div class="col-sm-6">
+            <input type="email" class="form-control" id="inputEmail3" placeholder="Bahan Bakar">
+          </div>
+        </div>
+        <div style="margin-bottom: 5px" class="form-group">
+          <label for="inputEmail3" class=" control-label col-sm-4">Jumlah Sumbu</label>
+          <div class="col-sm-6">
+            <input type="email" class="form-control" id="inputEmail3" placeholder="Jumlah Sumbu">
+          </div>
+        </div>
+        <div style="margin-bottom: 5px" class="form-group">
+          <label for="inputEmail3" class=" control-label col-sm-4">Jumlah Roda</label>
+          <div class="col-sm-6">
+            <input type="email" class="form-control" id="inputEmail3" placeholder="Jumlah Roda">
+          </div>
+        </div>
+        <div style="margin-bottom: 5px" class="form-group">
+          <label for="inputEmail3" class=" control-label col-sm-4">No. Sertifikat Uji Type</label>
+          <div class="col-sm-6">
+            <input type="email" class="form-control" id="inputEmail3" placeholder="No. Sertifikat Uji Type">
+          </div>
+        </div>
+      </form>
+    </div>
+    <br>
+
+  </div>  <!--end kontent 2  -->
+
+  <div style="overflow: auto; border: none;"  style="width: 100%; height: 100%"><!-- start Content 3 -->
+    <div class="col-md-12">
+      <form class="form-horizontal" col-sm-2>
+        <br>
+        <div style="margin-bottom: 5px" class="form-group">
+          <label for="inputEmail3" class=" control-label col-sm-2">No. Faktur</label>
+          <div class="col-sm-8">
+            <input type="email" class="form-control" id="inputEmail3" placeholder="No. Faktur">
+          </div>
+        </div>
+        <div style="margin-bottom: 5px" class="form-group">
+          <label for="inputEmail3" class=" control-label col-sm-2">Tanggal</label>
+          <div class="col-sm-8">
+            <input type="email" class="form-control" id="inputEmail3" placeholder="Tanggal">
+          </div>
+        </div>
+        <div style="margin-bottom: 5px" class="form-group">
+          <label for="inputEmail3" class=" control-label col-sm-2">ATPM / Importir</label>
+          <div class="col-sm-8">
+            <input type="email" class="form-control" id="inputEmail3" placeholder="ATPM / Importir">
+          </div>
+        </div>
+        <div style="margin-bottom: 5px" class="form-group">
+          <label for="inputEmail3" class=" control-label col-sm-2">No. PIB</label>
+          <div class="col-sm-8">
+            <input type="email" class="form-control" id="inputEmail3" placeholder="No. PIB">
+          </div>
+        </div>
+        <div style="margin-bottom: 5px" class="form-group">
+          <label for="inputEmail3" class=" control-label col-sm-2">No. SUT</label>
+          <div class="col-sm-8">
+            <input type="email" class="form-control" id="inputEmail3" placeholder="No. SUT">
+          </div>
+        </div>
+        <div style="margin-bottom: 5px" class="form-group">
+          <label for="inputEmail3" class=" control-label col-sm-2">No. TPT</label>
+          <div class="col-sm-8">
+            <input type="email" class="form-control" id="inputEmail3" placeholder="No. TPT">
+          </div>
+        </div>
+        <div style="margin-bottom: 5px" class="form-group">
+          <label for="inputEmail3" class=" control-label col-sm-2">No. Form A / B</label>
+          <div class="col-sm-8">
+            <input type="email" class="form-control" id="inputEmail3" placeholder="No. Form A / B">
+          </div>
+        </div>
+        <div style="margin-bottom: 5px" class="form-group">
+          <label for="inputEmail3" class=" control-label col-sm-2">Kantor Bea Cukai</label>
+          <div class="col-sm-8">
+            <input type="email" class="form-control" id="inputEmail3" placeholder="Kantor Bea Cukai">
+          </div>
+        </div>
+        <div style="margin-bottom: 5px" class="form-group">
+          <label for="inputEmail3" class=" control-label col-sm-2">Lain Lain</label>
+          <div class="col-sm-8">
+            <input type="email" class="form-control" id="inputEmail3" placeholder="Lain Lain">
+          </div>
+        </div>
+      </form>
+    </div>
+    <!-- end  konten 3  -->  </div>
+    <div style="overflow: auto; border: none;"  style="width: 100%; height: 100%">
+      <!-- start konten  4 -->
+      <div class="col-md-12">
+        <form class="form-horizontal" col-sm-2>
+          <br>
+          <div style="margin-bottom: 5px" class="form-group">
+            <label for="inputEmail3" class=" control-label col-sm-2">Dikeluarkan Oleh</label>
+            <div class="col-sm-8">
+              <input type="email" class="form-control" id="inputEmail3" placeholder="Dikeluarkan Oleh">
+            </div>
+          </div>
+          <div style="margin-bottom: 5px" class="form-group">
+            <label for="inputEmail3" class=" control-label col-sm-2">No. Register</label>
+            <div class="col-sm-8">
+              <input type="email" class="form-control" id="inputEmail3" placeholder="No. Register">
+            </div>
+          </div>
+        </form>
+      </div>
+      <!-- end konten 4 -->
+    </div>
+    <div>
+      Content 5
+    </div>
+  </div>
+
+  <?php
+  $this->load->view("index_js");
+  ?>
+
+</div>   <!--end Print tanda tanda terima container-fluid -->
